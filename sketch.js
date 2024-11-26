fdecf6becf90a89eaf2f45a3d1eb2ee8c93ad7e4
emng8 <emng888@gmail.com>
2024-11-26 14:15:50 -0600

    started making my grid, added necessary variables, and made a display grid function.

--- sketch.js
+++ sketch.js
@@ -1,10 +1,32 @@
-// Project Title
-// Your Name
+// Flow Free
+// Emily Ng
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let grid = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+const GRID_ROWS = grid.length;
+const GRID_COLUMNS = grid[0].length; 
+const cellSize = 50;
+
+const EMPTY_TILE = 0;
+const RED_DOT = 1;
+const BLUE_DOT = 2;
+const GREEN_DOT = 3;
+const YELLOW_DOT = 4;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,5 +35,17 @@ function setup() {
 function draw() {
   background(220);
 
-  circle(mouseX, mouseY, 100);
+}
+
+function mousePressed() {
+  if (mouseX && mouseY === ) {
+
+  }
+}
+
+function displayGrid() {
+  for (let y = 0; y < GRID_ROWS; y++) {
+    for (let x = 0; x < GRID_COLUMNS; x++) {
+    }
+  }
 }
