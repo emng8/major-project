86f9f51d96401d14e2116fd04aab09f6ced8a33f
emng8 <emng888@gmail.com>
2024-12-04 14:14:39 -0600

    got the colored squares to show on my grid and started to create the setup for the second level.

--- sketch.js
+++ sketch.js
@@ -6,8 +6,9 @@
 // - describe what you did to take this project "above and beyond"
 
 // just testing the concept of having an initial grid, the grid with the dots, and then having the solved grid
-// intial plain grid
-let gri = [
+
+// intial plain grid. nothing on it
+let emptyGrid = [
   [0, 0, 0, 0, 0], 
   [0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0],
@@ -15,6 +16,7 @@ let gri = [
   [0, 0, 0, 0, 0]
 ];
 
+// possible set up to try with the dots
 let grid = [
   [1, 2, 0, 0, 0], 
   [0, 0, 0, 1, 0],
@@ -23,7 +25,25 @@ let grid = [
   [0, 3, 0, 0, 4]
 ];
 
-let solvedgridOne = [
+// how the gridd should look like when it is solved
+let solvedGridOne = [
+  [1, 2, 2, 2, 2], 
+  [1, 1, 1, 1, 2],
+  [3, 4, 4, 2, 2],
+  [3, 3, 4, 2, 2],
+  [3, 3, 4, 4, 4]
+];
+
+let gridTwo = [
+  [, , 0, 0, 0, 0], 
+  [0, 0, 0, , 0, 0],
+  [, , 0, , 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, , 0, 0, , 0],
+  [0, , 0, 0, , 0]
+];
+
+let solvedGridTwo = [
   [1, 2, 2, 2, 2], 
   [1, 1, 1, 1, 2],
   [3, 4, 4, 2, 2],
@@ -42,7 +62,7 @@ const GREEN_DOT = 3;
 const YELLOW_DOT = 4;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500, 500);
 }
 
 function draw() {
@@ -55,19 +75,23 @@ function displayGrid() {
     for (let x = 0; x < GRID_COLUMNS; x++) {
       // displays red
       if (grid[y][x] === RED_DOT) {
-        fill("red");
+        fill("red"); 
+        square(x * cellSize, y * cellSize, cellSize);
       } 
       // displays blue
       else if (grid[y][x] === BLUE_DOT) {
         fill("blue");
+        square(x * cellSize, y * cellSize, cellSize);
       } 
       // displays green
       else if (grid[y][x] === GREEN_DOT) {
         fill("green");
+        square(x * cellSize, y * cellSize, cellSize);
       } 
       // displays yellow
       else if (grid[y][x] === YELLOW_DOT) {
         fill("yellow");
+        square(x * cellSize, y * cellSize, cellSize);
       }       
     }
   }
